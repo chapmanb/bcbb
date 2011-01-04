68a0511678e745fe6c8d43c4ff5d7c29917eceb2
Brad Chapman <chapmanb@50mail.com>
2011-01-04 13:55:12 -0500

    Shorten NCBI identifiers and check for missing IDs in GenBank to GFF conversion

--- gff/Scripts/gff/gff_to_genbank.py
+++ gff/Scripts/gff/gff_to_genbank.py
@@ -9,13 +9,36 @@ import os
 
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna
+from Bio import Seq
+
 from BCBio import GFF
 
 def main(gff_file, fasta_file):
     out_file = "%s.gb" % os.path.splitext(gff_file)[0]
-    fasta_input = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta", generic_dna))
+    fasta_input = SeqIO.to_dict(_fix_ncbi_id(
+        SeqIO.parse(fasta_file, "fasta", generic_dna)))
     gff_iter = GFF.parse(gff_file, fasta_input)
-    SeqIO.write(gff_iter, out_file, "genbank")
+    SeqIO.write(_check_gff(gff_iter), out_file, "genbank")
+
+def _fix_ncbi_id(fasta_iter):
+    """GenBank identifiers can only be 16 characters; try to shorten NCBI.
+    """
+    for rec in fasta_iter:
+        if len(rec.name) > 16 and rec.name.find("|") > 0:
+            new_id = [x for x in rec.name.split("|") if x][-1]
+            print "Warning: shortening NCBI name %s to %s" % (rec.id, new_id)
+            rec.name = new_id
+            yield rec
+
+def _check_gff(gff_iterator):
+    """Check GFF files before feeding to SeqIO to be sure they have sequences.
+    """
+    for rec in gff_iterator:
+        if isinstance(rec.seq, Seq.UnknownSeq):
+            print "Warning: FASTA sequence not found for '%s' in GFF file" % (
+                    rec.id)
+            rec.seq.alphabet = generic_dna
+        yield rec
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
