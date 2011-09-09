db7a5be3eb83eb93ad5c1f951e8a33deb4ada4cb
Valentine Svensson <valentine@nxn.se>
2011-09-09 11:25:00 +0200

    Adding files for testing transferring.

---
+++ nextgen/tests/test_transfer.py
@@ -0,0 +1,27 @@
+"""Testing using config files for transfer settings.
+"""
+import os
+import subprocess
+
+def test_analyze_finished_sqn():
+	"""Test running the script with the config files in the
+	sub directory tests/test_transfer_data as input.
+	"""
+	config_dir = os.path.join(os.path.dirnam(__file__), "test_transfer_data")
+	cl = ["analyze_finished_sqn.py", 
+		  os.path.join(config_dir, "universe_wsgi.ini"),
+		  os.path.join(config_dir, "post_process.yaml")]
+
+	subprocess.check_call(cl)
+
+def test_analyze_finished_sqn_transfer_info():
+	"""Test running the script with the config files in the
+	sub directory tests/test_transfer_data as input.
+	"""
+	config_dir = os.path.join(os.path.dirnam(__file__), "test_transfer_data")
+	cl = ["analyze_finished_sqn.py", 
+		  os.path.join(config_dir, "universe_wsgi.ini"),
+		  os.path.join(config_dir, "post_process.yaml"),
+		  os.path.join(config_dir, "transfer_info.yaml")]
+
+	subprocess.check_call(cl)
