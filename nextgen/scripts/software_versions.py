1bbf801fa5aa1f809cf8af9c9b620a0d6226ae5c
b97pla <pontus.larsson@scilifelab.se>
2011-09-14 14:54:59 +0200

    removed dependency on module system

--- nextgen/scripts/software_versions.py
+++ nextgen/scripts/software_versions.py
@@ -17,16 +17,23 @@ import logbook
 import subprocess
 import shlex
 import re
+import glob
+
 from optparse import OptionParser
 
-GIT_COMMIT_HASH=""
-
-LOG_NAME = os.path.splitext(os.path.basename(__file__))[0]
-log = logbook.Logger(LOG_NAME)
+# The name of the git repository (if any) where the python scripts are contained
+git_repo = 'bcbb'
 
 # A dictionary pointing to routines that know how to get the version info from a piece of software
 templates = {
-             'pdflatex': '_pdflatex_version'
+             'pdflatex': '_pdflatex_version',
+             'maq': '_maq_version',
+             'samtools': '_samtools_version',
+             'bowtie': '_bowtie_version',
+             'picard': '_picard_version',
+             'bwa': '_bwa_version',
+             'fastqc': '_fastqc_version',
+             'gatk': '_gatk_version'
              }
 
 def main(config_file):
@@ -39,19 +46,6 @@ def main(config_file):
     with open(config_file) as in_handle:
         config = yaml.load(in_handle)
     
-    # Get the list of currently loaded modules
-    modules = dict()
-    try:
-        args = shlex.split("modulecmd bash -t list")
-        output = subprocess.Popen(args, stderr=subprocess.PIPE).communicate()[1]
-        rows = output.decode('UTF-8').split("\n")
-        for row in output.decode('UTF-8').split("\n"):
-            pcs = row.split("/")
-            if (len(pcs) == 2):
-                modules[pcs[0].lower()] = pcs[1]
-    except Exception, e:
-        log.error("Could not get module list: %s" % e)
-    
     # Get the list of external software from the config file
     prog_version = dict()
     for name, executable in config.get("program",{}).items():
@@ -60,44 +54,71 @@ def main(config_file):
         
         # If the executable is a python script, get the path and get the git commit hash for the repository
         if os.path.splitext(executable)[1] == '.py':
-            version = _get_git_hash(executable)
-        # Else, if the software is in the list of loaded modules, get the version from there
-        elif name in modules:
-            version = modules[name]
+            version = _get_git_hash(git_repo)
         # Else, if we have a function that knows how to get the version info from the particular piece of software, call that
         elif name in templates:
             version = globals()[templates[name]](executable)
-        # Else, we don't know..
+        # Else, try the generic approach..
         else:
-            version = "N/A"
+            version = _generic_version(executable)
             
         prog_version[name] = version
         
     for name,version in prog_version.items():
         print "%s: version %s" % (name,version)
-        
-def _get_git_hash(script_name):
-    
-    if len(GIT_COMMIT_HASH) > 0:
-        return GIT_COMMIT_HASH
-    else:
-        return "N/A"
 
-def _generic_version(executable):
+def _get_output(executable,argument=''):
+    args = shlex.split("%s %s" % (executable,argument))
+    output = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    return output.decode('UTF-8')
+            
+def _get_git_hash(git_repo):
     
-    args = shlex.split("%s -v" % executable)
-    output = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
-    version = output.decode('UTF-8')
-    return version
+    hash_file = os.path.join(os.getenv('HOME'),".%s_commit" % git_repo)
+    commit = "N/A"
+    if os.path.exists(hash_file):
+        with open(hash_file) as hf:
+            commit = hf.read().strip()
+    return commit 
+
+def _generic_version(executable,argument='-v',regexp=r'Version:\s*(\S+)'):
     
-def _pdflatex_version(executable):
-    
-    output = _generic_version(executable)
-    m = re.search(r'(3\.14.*)',output.split("\n")[0])
+    try:
+        output = _get_output(executable,argument)
+    except Exception, e:
+        log.info("Exception caught when trying to determine software version for %s: %s" % (executable,e))
+        output = ""
+        
+    m = re.search(regexp,output,re.I)
     if m and len(m.groups()) > 0:
         return m.group(1)
     return "N/A"
-         
+
+    
+def _maq_version(executable):
+    return _generic_version(executable,'')
+def _samtools_version(executable):
+    return _generic_version(executable,'')
+def _bwa_version(executable):
+    return _generic_version(executable,'')
+def _pdflatex_version(executable):
+    return _generic_version(executable,'-v',r'(3\.14.*)') # The pdflatex version numbers are approaching pi...
+def _fastqc_version(executable):
+    return _generic_version(executable,'-v',r'v(\S+)')
+def _bowtie_version(executable):
+    return _generic_version(executable,'--version',r'bowtie\s+version\s+(\S+)')
+def _gatk_version(executable):
+    return _generic_version('java',"-jar %s --help" % os.path.join(executable,"GenomeAnalysisTK.jar"),r'\(GATK\)\s+v(\S+?),')    
+def _picard_version(picard_home_dir):
+    # Assume that the picard jarfile has the version number in the file name...
+    jars = glob.glob(os.path.join(picard_home_dir,'picard-*.jar'))
+    if len(jars) > 0:    
+        jarname = os.path.splitext(os.path.split(jars[0])[1])[0]
+        m = re.search(r'picard\-(\S+)',jarname,re.I)
+        if m and len(m.groups()) > 0:
+            return m.group(1)
+    return "N/A"
+
 if __name__ == "__main__":
     parser = OptionParser()
     (options, args) = parser.parse_args()
