2e495979a83ec1d10088458f0d0d43a161f8c782
Valentine Svensson <valentine@nxn.se>
2011-09-15 15:40:02 +0200

    Testing with processor clock in stead of time, just to be sure the file change content.

--- nextgen/tests/test_transfer.py
+++ nextgen/tests/test_transfer.py
@@ -91,11 +91,11 @@ def perform__remote_copy_test(transfer_function):
 	test_data = {}
 	for test_file in remote_info["to_copy"]:
 		with open("%s/%s" % (copy_dir, test_file), "w") as file_to_write:
-			# We just use seconds since epoch as test data, the important
+			# We just the current processor time as test data, the important
 			# part is that it will be different enough between tests just
 			# so we know we are not comparing with files copied in a 
 			# previous test during the assertion.
-			test_data[test_file] = str(time.time())
+			test_data[test_file] = str(time.clock())
 			file_to_write.write(test_data[test_file])
 
 	# Perform copy with settings
