733eee657e1182731223488e08e26bced1b22e33
Valentine Svensson <valentine@nxn.se>
2011-10-04 14:33:25 +0200

    Implemented several transfer protocol options

--- nextgen/bcbio/pipeline/storage.py
+++ nextgen/bcbio/pipeline/storage.py
@@ -9,12 +9,14 @@ import fabric.contrib.files as fabric_files
 from bcbio.pipeline import log
 from bcbio.log import create_log_handler
 
+
 def long_term_storage(remote_info, config_file):
     with open(config_file) as in_handle:
         config = yaml.load(in_handle)
     log_handler = create_log_handler(config, log.name)
     with log_handler.applicationbound():
         _copy_for_storage(remote_info, config)
+
 
 def _copy_for_storage(remote_info, config):
     """Securely copy files from remote directory to the storage server.
@@ -37,14 +39,52 @@ def _copy_for_storage(remote_info, config):
 
     if not fabric_files.exists(fc_dir):
         fabric.run("mkdir %s" % fc_dir)
-        
-    for fcopy in remote_info['to_copy']:
-        target_loc = os.path.join(fc_dir, fcopy)
-        if not fabric_files.exists(target_loc):
+
+    if protocol == "scp" or protocol == None:
+        for fcopy in remote_info['to_copy']:
+            target_loc = os.path.join(fc_dir, fcopy)
+            if not fabric_files.exists(target_loc):
+                target_dir = os.path.dirname(target_loc)
+                if not fabric_files.exists(target_dir):
+                    fabric.run("mkdir -p %s" % target_dir)
+
+                cl = ["scp", "-r", "%s@%s:%s/%s" % (
+                      remote_info["user"], remote_info["hostname"], remote_info["directory"],
+                      fcopy), target_loc]
+
+                log.debug(cl)
+                fabric.run(" ".join(cl))
+
+    elif protocol == "rsync":
+        for fcopy in remote_info['to_copy']:
+            target_loc = os.path.join(fc_dir, fcopy)
             target_dir = os.path.dirname(target_loc)
+
             if not fabric_files.exists(target_dir):
                 fabric.run("mkdir -p %s" % target_dir)
-            cl = ["scp", "-r", "%s@%s:%s/%s" % (
-                  remote_info["user"], remote_info["hostname"], remote_info["directory"],
-                  fcopy), target_loc]
+
+            if os.path.isdir("%s/%s" % (remote_info["directory"], fcopy)) and fcopy[-1] != "/":
+                fcopy += "/"
+
+            # Option -P --append should enable resuming progress on partial transfers
+            cl = ["rsync", "-craz", "-P", "--append", "%s@%s:%s/%s" %
+                  (remote_info["user"], remote_info["hostname"],
+                   remote_info["directory"], fcopy)]
+
+            log.debug(cl)
             fabric.run(" ".join(cl))
+
+    # Note: rdiff-backup doesn't have the ability to resume a partial transfer,
+    # and will instead transfer the backup from the beginning if it detects a partial
+    # transfer.
+    elif protocol == "rdiff-backup":
+        include = []
+        for fcopy in remote_info['to_copy']:
+            include.append("--include %s/%s" % (remote_info["directory"], fcopy))
+
+        cl = ["rdiff-backup", " ".join(include), "--exclude '**'", "%s@%s::%s" %
+              (remote_info["user"], remote_info["hostname"], remote_info["directory"]),
+              fc_dir]
+
+        log.debug(cl)
+        fabric.run(" ".join(cl))
