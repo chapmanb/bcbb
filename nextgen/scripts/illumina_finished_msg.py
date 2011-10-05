17d6cf20b70c2e7ca2a4da2892670eff25197bc4
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2011-10-05 10:51:58 +0200

    issue #21

--- nextgen/scripts/illumina_finished_msg.py
+++ nextgen/scripts/illumina_finished_msg.py
@@ -272,6 +272,7 @@ def _update_reported(msg_db, new_dname):
     """
     with open(msg_db, "a") as out_handle:
         out_handle.write("%s\n" % new_dname)
+        out_handle.close()
 
 def finished_message(tag_name, directory, files_to_copy, config):
     """Wait for messages with the give tag, passing on to the supplied handler.
