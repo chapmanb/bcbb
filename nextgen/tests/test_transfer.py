8b89253f761ca3cc72eb4e02e62873a1db4616a9
Valentine Svensson <valentine@nxn.se>
2011-10-07 16:04:13 +0200

    Reformatting the test_transfer.py a bit

--- nextgen/tests/test_transfer.py
+++ nextgen/tests/test_transfer.py
@@ -13,8 +13,10 @@ def _remove_transferred_files(remote_info, config):
     """Remove the files transferred in a previous test.
     """
     copy_to = os.path.realpath("test_transfer_data/copy_to")
-    with fabric.settings(host_string="%s@%s" % (config["store_user"], config["store_host"])):
-        rm_str = "rm -r %s/%s" % (copy_to, os.path.split(remote_info["directory"])[1])
+    with fabric.settings(host_string="%s@%s" % \
+         (config["store_user"], config["store_host"])):
+        rm_str = "rm -r %s/%s" % \
+         (copy_to, os.path.split(remote_info["directory"])[1])
         log.debug(rm_str)
         fabric.run(rm_str)
 
@@ -32,7 +34,8 @@ def get_transfer_function(transfer_config):
     return transfer_function
 
 
-def perform__copy_for_storage(transfer_function, protocol_config, remove_before_copy=True, should_overwrite=False):
+def perform__copy_for_storage(transfer_function, protocol_config, \
+        remove_before_copy=True, should_overwrite=False):
     """Sets up dictionaries simulating loaded remote_info and config
     from various sources. Then test transferring files with the function
     using the standard setting.
@@ -40,13 +43,15 @@ def perform__copy_for_storage(transfer_function, protocol_config, remove_before_
     Note that there need to be passphrase free SSH between this machine
     and what is set up as store_host in post_process.yaml.
 
-    The file post_process.yaml need the information "store_user" and "store_host".
+    The file post_process.yaml need the information "store_user" and
+    "store_host".
 
     transfer_function : function - The function to use for transferring
     the files.
         This function should accept the two dictionaries config and
         remote_info as parameters.
-    protocol_config : dictionary - containing the key "transfer_protocol" which give the string
+    protocol_config : dictionary - containing the key "transfer_protocol"
+    which give the string
     of a transfer protocol.
     """
     config = load_config("data/automated/post_process.yaml")
@@ -70,18 +75,19 @@ def perform__copy_for_storage(transfer_function, protocol_config, remove_before_
         test_file_path = "%s/%s" % (copy_dir, test_file)
         if not os.path.isdir(test_file_path):
             with open(test_file_path, "w") as file_to_write:
-                # We just use the current processor time as test data, the important
-                # part is that it will be different enough between tests just
-                # so we know we are not comparing with files copied in a
-                # previous test during the assertion.
+                # We just use the current processor time as test data, the
+                # important part is that it will be different enough between
+                # test just so we know we are not comparing with files copied
+                # in a previous test during the assertion.
                 test_data[test_file] = str(time.clock())
                 file_to_write.write(test_data[test_file])
 
     # Perform copy with settings
-    with fabric.settings(host_string="%s@%s" % (remote_info["user"], remote_info["hostname"])):
+    with fabric.settings(host_string="%s@%s" % \
+         (remote_info["user"], remote_info["hostname"])):
 
-        if fabric_files.exists("%s/%s" % (store_dir, os.path.split(copy_dir)[1])
-        ) and remove_before_copy:
+        if fabric_files.exists("%s/%s" % \
+           (store_dir, os.path.split(copy_dir)[1])) and remove_before_copy:
             _remove_transferred_files(remote_info, config)
 
         # Copy
@@ -89,13 +95,17 @@ def perform__copy_for_storage(transfer_function, protocol_config, remove_before_
 
     # Check of the copy succeeded
     for test_file in remote_info["to_copy"]:
-        test_file_path = "%s/%s/%s" % (store_dir, os.path.split(copy_dir)[1], test_file)
+        test_file_path = "%s/%s/%s" % \
+                         (store_dir, os.path.split(copy_dir)[1], test_file)
         # Did the files get copied correctly
         if os.path.isfile(test_file_path):
             with open(test_file_path, "r") as copied_file:
                 read_data = copied_file.read()
-                fail_string = "File copy failed: %s doesn't equal %s (for %s). Remove is %s and Overwrite is %s." % (
-                read_data, test_data[test_file], test_file_path, str(remove_before_copy), str(should_overwrite))
+                fail_string = "File copy failed: " + \
+                "%s doesn't equal %s (for %s). " + \
+                "Remove is %s and Overwrite is %s." % \
+                (read_data, test_data[test_file], test_file_path,
+                str(remove_before_copy), str(should_overwrite))
                 # Assertion that passes when:
                 #  - The files got copied if we removed the old files in the
                 # target directory before the copy.
@@ -103,7 +113,8 @@ def perform__copy_for_storage(transfer_function, protocol_config, remove_before_
                 # target directory if they where not supposed to overwrite.
                 #  - The new files did replace the old files in the target
                 # directory of we specified that this should happen.
-                assert (read_data == test_data[test_file]) == (remove_before_copy or should_overwrite), fail_string
+                assert (read_data == test_data[test_file]) == \
+                (remove_before_copy or should_overwrite), fail_string
         # Did the directories get copied correcty
         if os.path.isdir(test_file_path):
             pass
@@ -115,7 +126,8 @@ def test__copy_for_storage():
     """
     config = {}
     perform__copy_for_storage(_copy_for_storage, config)
-    perform__copy_for_storage(_copy_for_storage, config, remove_before_copy=False)
+    perform__copy_for_storage(_copy_for_storage, config,
+    remove_before_copy=False)
 
 
 def test__copy_for_storage_scp():
@@ -123,7 +135,8 @@ def test__copy_for_storage_scp():
     """
     config = {"transfer_protocol": "scp"}
     perform__copy_for_storage(_copy_for_storage, config)
-    perform__copy_for_storage(_copy_for_storage, config, remove_before_copy=False)
+    perform__copy_for_storage(_copy_for_storage, config,
+    remove_before_copy=False)
 
 
 def test__copy_for_storage_rsync():
@@ -131,7 +144,8 @@ def test__copy_for_storage_rsync():
     """
     config = {"transfer_protocol": "rsync"}
     perform__copy_for_storage(_copy_for_storage, config)
-    perform__copy_for_storage(_copy_for_storage, config, remove_before_copy=False, should_overwrite=True)
+    perform__copy_for_storage(_copy_for_storage, config,
+    remove_before_copy=False, should_overwrite=True)
 
 
 def test__copy_for_storage_rdiff_backup():
@@ -139,4 +153,5 @@ def test__copy_for_storage_rdiff_backup():
     """
     config = {"transfer_protocol": "rdiff-backup"}
     perform__copy_for_storage(_copy_for_storage, config)
-    perform__copy_for_storage(_copy_for_storage, config, remove_before_copy=False, should_overwrite=True)
+    perform__copy_for_storage(_copy_for_storage, config,
+    remove_before_copy=False, should_overwrite=True)
