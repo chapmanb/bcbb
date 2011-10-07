f961008d6ae55e692a9945e965b02b1a1882decc
Per Unneberg <per.unneberg@ki.se>
2011-10-07 18:31:26 +0200

    setup scilife symlinks for sample_delinery

--- nextgen/tests/test_sample_delivery.py
+++ nextgen/tests/test_sample_delivery.py
@@ -12,15 +12,27 @@ class SampleDeliveryTest(unittest.TestCase):
     """Deliver samples from bcbio-processed data to John Doe"""
     
     def setUp(self):
-        self.file_dir = os.path.join(os.path.dirname(__file__))
+        self.file_dir = os.path.dirname(os.path.abspath(__file__))
         self.proj_dir = os.path.join(self.file_dir, "projects", "j_doe_00_01")
         self.fcdir = os.path.join(os.path.dirname(__file__), "test_automated_output")
+        archive_dir  = os.path.join(os.path.dirname(__file__), "scilife", "archive", "110106_FC70BUKAAXX")
+        analysis_dir  = os.path.join(os.path.dirname(__file__), "scilife", "analysis")
         delivery_dir = os.path.join(self.proj_dir, "data", "110106_FC70BUKAAXX")
         self._install_config_data()
         if os.path.exists(delivery_dir):
             shutil.rmtree(delivery_dir)
         if not os.path.exists(self.proj_dir):
             os.makedirs(self.proj_dir)
+        if not os.path.exists(analysis_dir):
+            os.makedirs(analysis_dir)
+            src = os.path.join(file_dir, "test_automated_output")
+            dest = os.path.abspath(os.path.join(analysis_dir, "110106_FC70BUKAAXX"))
+            os.symlink(src, dest)
+        if not os.path.exists(archive_dir):
+            os.makedirs(archive_dir)
+            src = os.path.abspath(os.path.join(file_dir, "data", "automated", "run_info.yaml"))
+            dest = os.path.join(archive_dir, "run_info.yaml")
+            os.symlink(src, dest)
 
     def _install_config_data(self):
         loc_files = ['bowtie_indices.loc', 'bwa_index.loc', 'sam_fa_indices.loc']
