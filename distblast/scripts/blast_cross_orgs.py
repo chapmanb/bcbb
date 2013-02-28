ead89c79e59b1a14edcbff9f3c686ffdb0ed31ea
chapmanb <chapmanb@50mail.com>
2013-02-28 05:40:29 -0500

    Rewrite processing component to avoid using global file locks, which don't work consistently on Windows. Instead stream results in batches and write as finished

--- distblast/scripts/blast_cross_orgs.py
+++ distblast/scripts/blast_cross_orgs.py
@@ -13,14 +13,13 @@ Requires:
 import os
 import sys
 import csv
+import itertools
 import multiprocessing
 
 import yaml
 from Bio import SeqIO
 
 from bcbio.phylo import blast
-
-fupdate_lock = multiprocessing.Lock()
 
 def main(org_config_file, config_file):
     with open(config_file) as in_handle:
@@ -31,14 +30,37 @@ def main(org_config_file, config_file):
         os.makedirs(config['work_dir'])
     (_, org_names, db_refs) = blast.get_org_dbs(config['db_dir'],
             org_config['target_org'])
-    id_file, score_file = setup_output_files(org_config['target_org'],
-            org_names)
-    file_info = [id_file, score_file]
-    pool = multiprocessing.Pool(int(config['num_cores']))
+    id_file, score_file = setup_output_files(org_config['target_org'])
     with open(org_config['search_file']) as in_handle:
-        pool.map(_process_wrapper,
-                ((rec, db_refs, file_info, config['work_dir'], config.get("blast_cmd"))
-                    for rec in SeqIO.parse(in_handle, "fasta")))
+        with open(id_file, "w") as id_out_handle:
+            with open(score_file, "w") as score_out_handle:
+                id_writer = csv.writer(id_out_handle, dialect='excel-tab')
+                score_writer = csv.writer(score_out_handle, dialect='excel-tab')
+                header = [""] + org_names
+                id_writer.writerow(header)
+                score_writer.writerow(header)
+                _do_work(db_refs, in_handle, id_writer, score_writer, config)
+
+def _do_work(db_refs, in_handle, id_writer, score_writer, config):
+    cores = int(config["num_cores"])
+    pool = multiprocessing.Pool(cores)
+    for rec_group in partition_all(cores * 100, SeqIO.parse(in_handle, "fasta")):
+        for out in pool.imap(_process_wrapper,
+                             ((rec, db_refs, config['work_dir'], config.get("blast_cmd"))
+                              for rec in rec_group)):
+            id_writer.writerow([out["cur_id"]] + out["cmp_id"])
+            score_writer.writerow([out["cur_id"]] + out["cmp_score"])
+
+def partition_all(n, iterable):
+    """Split into lazy chunks of n size.
+    http://stackoverflow.com/questions/5129102/python-equivalent-to-clojures-partition-all
+    """
+    it = iter(iterable)
+    while True:
+        chunk = list(itertools.islice(it, n))
+        if not chunk:
+            break
+        yield chunk
 
 def _process_wrapper(args):
     try:
@@ -46,28 +68,20 @@ def _process_wrapper(args):
     except KeyboardInterrupt:
         raise Exception
 
-def process_blast(rec, db_refs, file_info, tmp_dir, blast_cmd):
+def process_blast(rec, db_refs, tmp_dir, blast_cmd):
     """Run a BLAST writing results to shared files.
     """
     cur_id, id_info, score_info = blast.blast_top_hits(rec.id, rec.format("fasta"),
             db_refs, tmp_dir, blast_cmd)
-    with fupdate_lock:
-        id_file, score_file = file_info
-        for fname, fvals in [(id_file, id_info), (score_file, score_info)]:
-            with open(fname, "a") as out_handle:
-                writer = csv.writer(out_handle, dialect='excel-tab')
-                writer.writerow([cur_id] + fvals)
-        print cur_id
+    print cur_id
+    return {"cmp_id": id_info,
+            "cmp_score": score_info,
+            "cur_id": cur_id}
 
-def setup_output_files(target_org, cmp_orgs):
+def setup_output_files(target_org):
     base = target_org.replace(" ", "_")
     id_file = "%s-ids.tsv" % base
     eval_file = "%s-scores.tsv" % base
-    for fname in [id_file, eval_file]:
-        with open(fname, "w") as out_handle:
-            writer = csv.writer(out_handle, dialect="excel-tab")
-            header = [""] + cmp_orgs
-            writer.writerow(header)
     return id_file, eval_file
 
 if __name__ == "__main__":
--- distblast/setup.py
+++ distblast/setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "bcbio-phyloblast",
-      version = "0.1.1",
+      version = "0.1.2",
       author = "Brad Chapman",
       author_email = "chapmanb@50mail.com",
       description = "Distributed BLAST for gene phylogeny estimation",
