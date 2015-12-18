ca7d931582278bbbaecbf770f200ae6341a89bf4
chapmanb <chapmanb@50mail.com>
2015-12-18 10:48:18 -0500

    Rough scripts for plotting circos-style plots of structural variants

---
+++ svplot/extract_svs.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""Extract structural variations in regions of interest into tab delimited files for circos plots.
+"""
+import csv
+import os
+import subprocess
+
+import pysam
+
+def main():
+    samples = {"H1047R": ["H1047R-2-3", "H1047R-2-7", "H1047R-2-9"],
+               "MCF7": ["MCF71_4", "MCF71_8", "MCF72_5"]}
+    callers = ["lumpy", "manta"]
+    chroms = {"H1047R": ["8"],
+              "MCF7": ["18", "22"]}
+    allowed_chroms = set([str(x) for x in range(1, 23)])
+    for project, samples in samples.items():
+        out_file = "%s-svs.csv" % (project)
+        region_bed = "%s-ends.bed" % (project)
+        with open(out_file, "w") as out_handle:
+            with open(region_bed, "w") as region_out:
+                writer = csv.writer(out_handle)
+                writer.writerow(["chrom1", "start1", "end1", "chrom2", "start2", "end2", "sample", "caller", "svtype"])
+                for sample in samples:
+                    for ci, caller in enumerate(callers):
+                        sv_file = os.path.join("inputs", "%s-%s.vcf.gz" % (sample, caller))
+                        for p1, p2, svtype in parse_svs(sv_file):
+                            if True:
+                            #if p1[0] in chroms[project] or p2[0] in chroms[project]:
+                                if p1[0] in allowed_chroms and p2[0] in allowed_chroms:
+                                    writer.writerow(p1 + p2 + [sample, caller, svtype])
+                                    if ci == 0:
+                                        for (chrom, start, end) in [p1, p2]:
+                                            region_out.write("%s\t%s\t%s\t%s\n" % (chrom, start - 1, end, sample))
+        filter_shared(out_file, region_bed)
+        # noshared_file = "%s-noshared%s" % os.path.splitext(out_file)
+        # with open(noshared_file, "w") as out_handle:
+        #     writer = csv.writer(out_handle)
+        #     writer.writerow(["chrom1", "start1", "end1", "chrom2", "start2", "end2", "sample", "caller", "svtype"])
+        #     for row in filter_shared(out_file, region_bed):
+        #         writer.writerow(row)
+
+def filter_shared(in_file, region_bed):
+    cmd = "sort -V -k1,1 -k2,2n {region_bed} | bedtools merge -i - -d 10000 -c 4 -o count_distinct"
+    overlaps = subprocess.check_output(cmd.format(**locals()), shell=True)
+    shared = 0
+    noshared = 0
+    for line in overlaps.split("\n"):
+        if line.strip():
+            if int(line.strip().split()[-1]) > 1:
+                shared += 1
+            else:
+                noshared += 1
+    print shared, noshared
+    return []
+
+def parse_svs(in_file):
+    bnds = {}
+    min_su_bnd = 25
+    min_su_other = 25
+    for rec in pysam.VariantFile(in_file):
+        if passes(rec):
+            if rec.info["SVTYPE"] == "BND":
+                if rec.info.get("SU") is None or rec.info["SU"] > min_su_bnd:
+                    p = [rec.chrom.replace("chr", ""), rec.start, rec.start]
+                    if rec.id in bnds:
+                        p_o = bnds[rec.id]
+                        yield p_o, p, rec.info["SVTYPE"]
+                        del bnds[rec.id]
+                    else:
+                        mate_id = rec.info.get("MATEID")
+                        bnds[mate_id] = p
+            else:
+                if rec.info.get("SU") is None or rec.info["SU"] > min_su_other:
+                    p1 = [rec.chrom.replace("chr", ""), rec.start, rec.start]
+                    p2 = [rec.chrom.replace("chr", ""), rec.info["END"], rec.info["END"]]
+                    yield p1, p2, rec.info["SVTYPE"]
+
+def passes(rec):
+    if rec.filter.keys() == 0 or rec.filter.keys()[0] == "PASS":
+        if not rec.samples[0].get("GT"):
+            return True
+        elif list(set(rec.samples[0].get("GT"))) != ["N"]:
+            return True
+    return False
+
+if __name__ == "__main__":
+    main()
---
+++ svplot/plot_sv_circos.R
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+
+# Plot circos style representation of structural variants using ggbio
+
+library(biovizBase)
+library(GenomicRanges)
+library(ggbio)
+library(ggplot2)
+library(stringr)
+library(RColorBrewer)
+library(grid)
+library(gridExtra)
+
+data("CRC", package="biovizBase")
+ideo <- hg19sub
+
+args <- commandArgs(trailingOnly=TRUE)
+in.file <- args[1]
+out.file <- str_c(str_split(in.file, "[.]")[[1]][1], ".pdf")
+
+sv.df = read.csv(in.file, header=T, stringsAsFactors=FALSE)
+sv.gr = GRanges(sv.df$chrom1, IRanges(sv.df$start1, sv.df$end1),
+                svtype=sv.df$svtype, sample=sv.df$sample,
+                to.gr=GRanges(sv.df$chrom2, IRanges(sv.df$start2, sv.df$end2)))
+seqlevels(sv.gr) <- seqlevels(ideo)
+seqlengths(sv.gr) <- seqlengths(ideo)
+seqlevels(sv.gr$to.gr) <- seqlevels(ideo)
+seqlengths(sv.gr$to.gr) <- seqlengths(ideo)
+
+print(sv.gr)
+svtypes <- unique(as.character(values(sv.gr)$svtype))
+print(svtypes)
+
+sv.grl <- split(sv.gr, values(sv.gr)$sample)
+crc.lst <- lapply(sv.grl, function(gr.cur){
+  print(unique(as.character(values(gr.cur)$sample)))
+  cols <- c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c")
+  #cols <- RColorBrewer::brewer.pal(length(svtypes), "Set2")[length(svtypes):1]
+  names(cols) <- svtypes
+  p <- ggbio() + circle(gr.cur, geom="link", linked.to="to.gr", aes(color=svtype)) +
+                 circle(ideo, geom="text", aes(label=seqnames), vjust=0, size=4) +
+                 scale_color_manual(values = cols)  +
+                 labs(title = (unique(values(gr.cur)$sample)))
+})
+
+pdf(file=out.file, width=8, height=8)
+print(crc.lst[1])
+print(crc.lst[2])
+print(crc.lst[3])
+#arrangeGrobByParsingLegend(crc.lst, legend.idx = 1, ncol = 3)
+dev.off()
